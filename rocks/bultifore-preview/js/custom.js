5dc988f1f91f1202391c26ad8be71552a01a8298
unknown <richard.okoye@netopng.com>
2020-11-02 17:35:39 +0100

    dynamic footer and header and design dashboard forms

---
+++ rocks/bultifore-preview/js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    // var header = $("#nav");
+    // console.log(header);
+    $("#nav").load("../../layout/header.html");
+    $("#footer").load("../../layout/footer.html");
+
+
+});
+
+
+// $(document).ready(function () {
+//     var header = $("#nav");
+//     console.log(header);
+//     $("#nav").load("")
+// });
+
